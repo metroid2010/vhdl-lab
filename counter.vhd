5a18a7a54bbc6c58bb34bd3c2d903c09e0c4932a
metroid2010 <metroid2010@gmail.com>
2018-10-01 15:14:25 +0200

    added sense to counter

--- counter.vhd
+++ counter.vhd
@@ -1,6 +1,8 @@
 -- first part lab sessions - digital electronics
 -- programming a microcontroller for an elevator
 -- counter 0-3
+-- 2 bit output, invertible count (sense signal)
+
 
 library ieee;
 use ieee.std_logic_1164.all;
@@ -8,35 +10,37 @@ use ieee.numeric_std.all;
 
 entity counter3 is
 port (
-    reset,clk,enable : in std_logic;
+    reset,clk,enable,sense: in std_logic; --sense: 1 when up, 0 when down
     count : out unsigned(1 downto 0);
     carry_out : out std_logic
     );
 end counter3;
 
 architecture a_counter of counter3 is
-    signal c: unsigned(1 downto 0);
+    signal s_count: unsigned(1 downto 0);
 begin
     process (reset, clk, enable)
     begin
         if reset = '1' or enable = '0' then
-            c <= "00";
+            s_count <= "00";
         elsif clk'event and clk='1' then
             if c = "11" then
-                c <= "00";
-            else
-                c <= c + 1;
+                s_count <= "00";
+            elsif sense = '1' then
+                s_count <= s_count + 1;
+            elsif sense = '0' then
+                s_count <= s_count - 1;
             end if;
         end if;
     end process;
-    process (c)
+    process (s_count)
     begin
-        if c = "11" then
+        if s_count = "11" then
             carry_out <= '1';       
         else
             carry_out <= '0';
         end if;
     end process;
-    count <= c;
+    count <= s_count;
 end a_counter;
 
